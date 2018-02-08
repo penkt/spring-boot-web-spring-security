b7ef1f638d4ece795efa374700720648d8e35ad2
penket <1519619451@qq.com>
2018-02-08 18:31:22 +0800

    添加用户登录Listener

---
+++ src/main/java/com/pengkey/listener/UserLoginListener.java
@@ -0,0 +1,17 @@
+package com.pengkey.listener;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class UserLoginListener implements HttpSessionListener{
+
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+
+    }
+}
